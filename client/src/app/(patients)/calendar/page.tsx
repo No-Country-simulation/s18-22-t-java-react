2e1f433b45a44196e48782fb614f45525b6326ce
jwilliam96 <jwsa.1996@gmail.com>
2024-10-10 13:27:41 -0500

    calendar

---
+++ client/src/app/(patients)/calendar/page.tsx
@@ -0,0 +1,14 @@
+
+import Calendar from "@/components/calendar/Calendar";
+
+export default function CalendarPage() {
+
+
+    return (
+        <div >
+            <div className="max-w-[1200px] mx-auto px-6 my-10">
+                <Calendar />
+            </div>
+        </div>
+    );
+}
