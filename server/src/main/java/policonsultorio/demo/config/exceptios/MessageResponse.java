d3ec7d3b7688c6d7cf0bf3086187bfc143cbc033
Marcos Paz Goncheff <69874820+marcosep192000@users.noreply.github.com>
2024-10-13 18:56:06 -0300

    :sparkles: feat : add exceptions

---
+++ server/src/main/java/policonsultorio/demo/config/exceptios/MessageResponse.java
@@ -0,0 +1,10 @@
+package policonsultorio.demo.config.exceptios;
+
+import lombok.*;
+
+@Builder
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+public class MessageResponse {
+	private String messageResponse;
+}
