5569952bdaae64f06b7e92f756defd3cc084f43d
Marcos Paz Goncheff <69874820+marcosep192000@users.noreply.github.com>
2024-10-13 20:17:42 -0300

    :fire: feat : add entity Doctor

--- server/src/main/java/policonsultorio/demo/entity/Doctor.java
+++ server/src/main/java/policonsultorio/demo/entity/Doctor.java
@@ -4,10 +4,14 @@ import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @Entity
 public class Doctor extends User {
 	private String specialization;
 	private String licenseNumber;
+//	private List<WorkingHous> workingHousList = new List<WorkingHous>();
+//  private List<Appointments> appointmentsList = new List<Appointments>();
 }
