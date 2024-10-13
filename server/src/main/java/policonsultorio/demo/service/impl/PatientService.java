d85c24fba40bdefb8ec39ac22455075c4309822a
Leonel Amaya <omar.mejia2099@gmail.com>
2024-10-13 12:26:04 -0500

    feat: implement logic to CRUD operations of Patient entity
    Se crearon las operaciones crear paciente, listar pacientes y buscar paciente por id.

--- server/src/main/java/policonsultorio/demo/service/impl/PatientService.java
+++ server/src/main/java/policonsultorio/demo/service/impl/PatientService.java
@@ -1,8 +1,30 @@
 package policonsultorio.demo.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import policonsultorio.demo.entity.Patient;
+import policonsultorio.demo.repository.PatientRepository;
 import policonsultorio.demo.service.IPatientService;
+
+import java.util.List;
 
 @Service
 public class PatientService implements IPatientService {
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @Override
+    public Patient save(Patient patient) {
+        return patientRepository.save(patient);
+    }
+
+    @Override
+    public Patient getPatientById(int id) {
+        return patientRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Patient> getAllPatients() {
+        return patientRepository.findAll();
+    }
 }
