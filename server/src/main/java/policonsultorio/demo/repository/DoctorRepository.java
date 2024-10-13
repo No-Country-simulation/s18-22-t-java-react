03fcfd6cf96fe5031fcdfdfa88962003af221825
Marcos Paz Goncheff <69874820+marcosep192000@users.noreply.github.com>
2024-10-13 20:22:06 -0300

    :fire: Docto's repository  added

--- server/src/main/java/policonsultorio/demo/repository/DoctorRepository.java
+++ server/src/main/java/policonsultorio/demo/repository/DoctorRepository.java
@@ -3,5 +3,5 @@ package policonsultorio.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import policonsultorio.demo.entity.Doctor;
 
-public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
 }
