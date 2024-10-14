316cc6d018ce46354b9297de2471eb6b6e9ea275
jwilliam96 <jwsa.1996@gmail.com>
2024-10-14 08:12:10 -0500

    alert dialog

--- client/src/app/(patients)/dashboard/page.tsx
+++ client/src/app/(patients)/dashboard/page.tsx
@@ -1,38 +1,37 @@
 
 export default function DashboardPage() {
+
     return (
         <div className="max-w-[1400px] mx-auto px-10 my-16">
-            <div className="grid grid-cols-2 gap-10">
 
-                {/* TURNO  */}
-                <div >
-                    <h2 className="text-2xl font-semibold">Bienvenida, Mica!</h2>
+            <h2 className="text-2xl font-semibold">Te damos la bienvenida Mica!</h2>
 
-                    <form className="flex flex-col gap-2 max-w-[500px] my-10">
+            <div className="flex flex-col gap-2 my-10">
+                <h3>Tus turnos ya agendados:</h3>
 
-                        <h3>Próximos turnos</h3>
+                {/* RESERVA DE CITAS  */}
+                {Array.from({ length: 2 }).map((item, index) => (
+                    <div key={index} className="flex gap-4 items-center bg-gray-100 p-4 max-w-max">
+                        <div className="size-20 shrink-0 rounded-full bg-gray-300" />
 
-                        <input className="bg-gray-300 p-2" type="text" />
-                        <input className="bg-gray-300 p-2" type="text" />
-                        <span className="border max-w-max self-end px-2 py-1">Mis turnos</span>
-                    </form>
+                        <div className="shrink-0">
+                            <h2>Dermatología</h2>
+                            <span >Dra. Mónica Gonzalez</span>
 
-                    <button className="bg-gray-300 py-5 px-24">Reserva un turno</button>
+                            <p className="mt-2">24/10/224</p>
+                            <p>Clínica Pueyrredón - Jujuy 2176 </p>
+                        </div>
 
-                </div>
+                        <button className="bg-zinc-400/70 px-8 py-2 rounded-md ml-10">Cancelar</button>
+                        <button className="bg-zinc-400/70 px-8 py-2 rounded-md">Reprogramar</button>
 
-                {/* RECOMENDACIONES */}
-                <div className="max-w-[500px] justify-self-center w-full space-y-8">
-                    <div className="grid place-content-center text-center bg-gray-200 h-[150px]">
-                        <p>Recomendaciones de consulta de rutina <br />ej: chequeo anual</p>
                     </div>
-
-                    <div className="grid place-content-center text-center bg-gray-200 h-[150px]">
-                        <p>Recomendaciones de consulta de rutina <br />ej: chequeo anual</p>
-                    </div>
-                </div>
-
+                ))}
             </div>
+
+            <button className="bg-gray-300 py-5 px-24">Reserva un turno</button>
+
+
         </div>
     );
 }
