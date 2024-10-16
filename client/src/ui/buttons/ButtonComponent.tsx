99a3def505ad52ee446166de01e06f931f680fd1
lazaronazareno <lazaronazareno@gmail.com>
2024-10-16 19:09:46 -0300

    feat(frontend): :lipstick: colors added to tailwind config and update buttonComponent

--- client/src/ui/buttons/ButtonComponent.tsx
+++ client/src/ui/buttons/ButtonComponent.tsx
@@ -6,11 +6,11 @@ interface Props {
 }
 
 export function ButtonComponent({ className, size, variant, text }: Props) {
-  const style = variant === 'light' ? 'bg-white text-[#004784]' : 'bg-[#004784] text-white '
+  const style = variant === 'light' ? 'bg-white text-secondaryBlue-500' : 'bg-secondaryBlue-500 text-white '
   const sizing = size === 'normal' ? 'text-lg w-[220px] h-16' : 'text-[32px] w-[419px] h-24'
 
   return (
-    <button type="button" className={`rounded-[64px] border border-[#004784] ${className} ${style} ${sizing}`}>
+    <button type="button" className={`rounded-[64px] border border-secondaryBlue-500 ${className} ${style} ${sizing}`}>
       {text}
     </button>
   )
