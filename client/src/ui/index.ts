a2d7a966a86365fe7187185cf3c98ee6716dedcf
jwilliam96 <jwsa.1996@gmail.com>
2024-10-15 23:17:17 -0500

    select

--- client/src/ui/index.ts
+++ client/src/ui/index.ts
@@ -1,3 +1,4 @@
 export { RegisterForm } from "./auth/RegisterForm"
 export { LoginForm } from "./auth/LoginForm"
 export { TopMenu } from "./dashboard/TopMenu"
+export { Select } from "./clinic/Select"
