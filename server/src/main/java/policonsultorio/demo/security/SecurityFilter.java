f61e1106aefab4205e44f14e7d48235968163ebc
lionel staricoff <staricofflionel@hotmail.com>
2024-10-17 10:55:23 -0500

    fix: add class HttpServletRequestWrapper2.java to Apply Logic in Security Filte, modify class securityFilter and add pom dependency json

--- server/src/main/java/policonsultorio/demo/security/SecurityFilter.java
+++ server/src/main/java/policonsultorio/demo/security/SecurityFilter.java
@@ -5,15 +5,19 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import policonsultorio.demo.entity.User;
 import policonsultorio.demo.repository.UserRepository;
+import policonsultorio.demo.service.UserService;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
@@ -23,21 +27,35 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Autowired
     private UserRepository userRepository;
+
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        var authHeader = request.getHeader("Authorization");
-        if (authHeader != null) {
-            var token = authHeader.replace("Bearer", "");
-            var nombreUsuario = tokenService.getSubject(token);
 
-            if (nombreUsuario != null){
-                UserDetails usuario = userRepository.findByNameUserDetails(nombreUsuario);
-                var authentication = new UsernamePasswordAuthenticationToken(usuario,null,
-                        usuario.getAuthorities());
-                SecurityContextHolder.getContext().setAuthentication(authentication);
+        {
+//refactor
+            HttpServletRequestWrapper2 wrappedRequest = new HttpServletRequestWrapper2(request);
+            String body = wrappedRequest.getBody();
+            // Parse the body to extract the user ID
+            JSONObject jsonObject = new JSONObject(body);
+            String userId = jsonObject.getString("Id");
+
+
+            Optional<User> userDb = userRepository.findById(Long.valueOf(userId));
+            User user = userDb.get();
+            if (user != null) {
+                var token = "";//buscar el jwt en el service
+                var nombreUsuario = tokenService.getSubject(token);
+
+                if (nombreUsuario != null) {
+                    UserDetails usuario = userRepository.findByNameUserDetails(nombreUsuario);
+                    var authentication = new UsernamePasswordAuthenticationToken(usuario, null,
+                            usuario.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+
             }
-
+            filterChain.doFilter(request, response);
         }
-        filterChain.doFilter(request,response);
     }
 }
