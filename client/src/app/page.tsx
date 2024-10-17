81c1e61d5c6c766a98507bb52c2734cf962af1db
lazaronazareno <lazaronazareno@gmail.com>
2024-10-17 10:48:01 -0300

    auth layout, fix main layout

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,11 +2,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import HomeImage from '/public/images/home.png'
-import { ButtonComponent } from "@/ui";
+import { ButtonComponent, Header } from "@/ui";
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <div>
         <div className="relative w-full overflow-hidden">
           <div className="flex transition-transform ease-out duration-500">
