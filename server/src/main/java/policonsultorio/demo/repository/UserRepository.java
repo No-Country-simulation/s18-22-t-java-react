dc6032b8790bc3805e950783459b83c74116b338
lionel staricoff <staricofflionel@hotmail.com>
2024-10-17 01:33:02 -0500

    fix: repair query that serch userdetails

--- server/src/main/java/policonsultorio/demo/repository/UserRepository.java
+++ server/src/main/java/policonsultorio/demo/repository/UserRepository.java
@@ -2,11 +2,14 @@ package policonsultorio.demo.repository;
 
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.userdetails.UserDetails;
 import policonsultorio.demo.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByName(@NotBlank String name);
 
-    UserDetails findByNameUserDetails(String nombreUsuario);
+    @Query(value = "SELECT * FROM user WHERE name = :name",nativeQuery = true)
+    UserDetails findByNameUserDetails(@Param("name") String name);
 }
