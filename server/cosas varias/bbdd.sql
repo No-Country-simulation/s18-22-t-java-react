da82932340b0ca6d8c26ce4eb535720c1138f786
lionel staricoff <staricofflionel@hotmail.com>
2024-10-23 16:13:36 -0500

    fix: change validation login between name for email and validation email

--- server/cosas varias/bbdd.sql
+++ server/cosas varias/bbdd.sql
@@ -2,7 +2,7 @@ DROP DATABASE if EXISTS clinica;
 CREATE DATABASE clinica CHARACTER SET utf8 COLLATE UTF8_GENERAL_CI;
 USE clinica;
 
-DROP TABLE  user;
+
 CREATE TABLE user (
 	id BIGINT UNSIGNED  AUTO_INCREMENT  PRIMARY KEY,
     name VARCHAR(250) UNIQUE NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE user (
     email VARCHAR(250)  NOT NULL,
     img TEXT,
     phone VARCHAR(250)  NOT NULL,
-     rol ENUM('MEDIC','PACIENT'),
+     rol ENUM('MEDIC','PATIENT'),
      initial_date DATE NOT NULL,
 	  active BOOLEAN DEFAULT TRUE 
 );
@@ -26,7 +26,9 @@ CREATE TABLE authorizarion (
 #ALTER TABLE user ADD  initial_date DATE NOT NULL;
 
 SELECT * FROM user;
-SELECT * FROM authorizarion;
+SELECT * FROM authorization;
 SELECT * FROM doctor;
+SELECT * FROM patients;
 describe doctor;
 describe user;
+describe authorization;
