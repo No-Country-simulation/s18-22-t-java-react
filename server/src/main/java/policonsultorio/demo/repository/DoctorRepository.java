f2db78f7b44c14b60a52aa71282ba8ae160e041c
Marcos Paz Goncheff <69874820+marcosep192000@users.noreply.github.com>
2024-10-24 20:09:40 -0300

    :fire: refactor : DoctorRepository

--- server/src/main/java/policonsultorio/demo/repository/DoctorRepository.java
+++ server/src/main/java/policonsultorio/demo/repository/DoctorRepository.java
@@ -1,7 +1,11 @@
 package policonsultorio.demo.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import policonsultorio.demo.dto.response.DoctorResponse;
 import policonsultorio.demo.entity.Doctor;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
+	Page<Doctor> findByNameContainingIgnoreCase(String name, Pageable pageable);
 }
