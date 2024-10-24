37ff91f72ae89af672d154c2b3844f3cabd7b8ed
lionel staricoff <staricofflionel@hotmail.com>
2024-10-23 19:15:28 -0500

    fix: ADD put clinic

--- server/src/main/java/policonsultorio/demo/controller/ClinicController.java
+++ server/src/main/java/policonsultorio/demo/controller/ClinicController.java
@@ -1,11 +1,14 @@
 package policonsultorio.demo.controller;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import policonsultorio.demo.dto.clinic.RequestClinic;
 import policonsultorio.demo.dto.clinic.ResponseClinic;
+import policonsultorio.demo.dto.clinic.ResponseClinicUpdate;
 import policonsultorio.demo.repository.ClinicRepository;
 import policonsultorio.demo.service.ClinicService;
 
@@ -24,6 +27,16 @@ public class ClinicController {
         try {
             return ResponseEntity.status(HttpStatus.CREATED).body(clinicService.register(responseClinic));
         } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", e.getMessage()));
+
+        }
+    }
+
+    @PutMapping(value = "/{id}", produces = "application/json")
+    public ResponseEntity<?> updateClinic(@PathVariable Long id, @RequestBody @Valid ResponseClinicUpdate responseClinic){
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(clinicService.update(id, responseClinic));
+        }catch (Exception e){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", e.getMessage()));
 
         }
--- server/src/main/java/policonsultorio/demo/service/ClinicService.java
+++ server/src/main/java/policonsultorio/demo/service/ClinicService.java
@@ -1,10 +1,12 @@
 package policonsultorio.demo.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import policonsultorio.demo.dto.clinic.RequestClinic;
 import policonsultorio.demo.dto.clinic.ResponseClinic;
+import policonsultorio.demo.dto.clinic.ResponseClinicUpdate;
 import policonsultorio.demo.entity.Clinic;
 import policonsultorio.demo.repository.ClinicRepository;
 
@@ -16,7 +18,22 @@ public class ClinicService {
     private ClinicRepository clinicRepository;
 
     public RequestClinic register(ResponseClinic responseClinic) {
-     Clinic clinic =   clinicRepository.save(new Clinic(responseClinic));
-     return new RequestClinic(clinic.getId(),new ResponseClinic(clinic));
+        Clinic clinic = clinicRepository.save(new Clinic(responseClinic));
+        return new RequestClinic(clinic.getId(), new ResponseClinic(clinic));
+    }
+
+    public RequestClinic update(Long id, ResponseClinicUpdate responseClinic) {
+
+        Clinic clinic = clinicRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("clinic not found"));
+
+        if (!responseClinic.name().isBlank()) clinic.setName(responseClinic.name());
+        if (!responseClinic.address().isBlank()) clinic.setAddress(responseClinic.address());
+        if (!responseClinic.cuit().isBlank()) clinic.setCuit(responseClinic.cuit());
+        if (!responseClinic.phone().isBlank()) clinic.setPhone(responseClinic.phone());
+        if (!responseClinic.description().isBlank()) clinic.setDescription(responseClinic.description());
+        if (!responseClinic.vlinicImage().isBlank()) clinic.setVlinicImage(responseClinic.vlinicImage());
+
+        var clinicDb = clinicRepository.save(clinic);
+        return new RequestClinic(clinicDb.getId(), new ResponseClinic(clinicDb));
     }
 }
