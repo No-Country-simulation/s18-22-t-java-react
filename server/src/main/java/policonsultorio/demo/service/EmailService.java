a99f11ae7e438c30f9e47c8fc4ec9608def76eef
lionel staricoff <staricofflionel@hotmail.com>
2024-10-27 16:34:30 -0500

    fix: add value from aplication properties

--- server/src/main/java/policonsultorio/demo/service/EmailService.java
+++ server/src/main/java/policonsultorio/demo/service/EmailService.java
@@ -1,6 +1,7 @@
 package policonsultorio.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -13,12 +14,14 @@ public class EmailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    @Value("${spring.mail.username}")
+    private String emailFrom;
     public boolean sendEmail(DtoEmail dtoEmail) {
         Boolean response = false;
         try {
             SimpleMailMessage mesage = new SimpleMailMessage();
             mesage.setTo(dtoEmail.email());
-            mesage.setFrom("stjavareact71@gmail.com");
+            mesage.setFrom(emailFrom);
             mesage.setSubject(dtoEmail.titulo());
             mesage.setText(dtoEmail.mensaje());
 
--- server/src/test/java/policonsultorio/demo/controller/emailControllerTest.java
+++ server/src/test/java/policonsultorio/demo/controller/emailControllerTest.java
@@ -1,6 +1,5 @@
 package policonsultorio.demo.controller;
 
-import jakarta.validation.constraints.NotBlank;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +8,9 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.*;
-import policonsultorio.demo.dto.clinic.RequestClinic;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class emailControllerTest {
