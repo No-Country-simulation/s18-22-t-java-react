e827bf9938647c6dd454e4aea8764a5bb70e9d91
Leonel Amaya <omar.mejia2099@gmail.com>
2024-10-27 11:46:06 -0500

    feat: create query for the waiting list search

--- server/src/main/java/policonsultorio/demo/repository/WaitingQueueRepository.java
+++ server/src/main/java/policonsultorio/demo/repository/WaitingQueueRepository.java
@@ -2,8 +2,13 @@ package policonsultorio.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import policonsultorio.demo.entity.Doctor;
 import policonsultorio.demo.entity.WaitingQueue;
+import policonsultorio.demo.enums.QueueStatus;
+
+import java.util.List;
 
 @Repository
 public interface WaitingQueueRepository extends JpaRepository<WaitingQueue, Long> {
+    List<WaitingQueue> findByDoctorAndStatus(Doctor doctor, QueueStatus status);
 }
