78fcb17e3a2f66b5557a0ae0aa2584d09fd1cdf6
An7h0ny1 <91570972+An7h0ny1@users.noreply.github.com>
2024-10-27 19:56:54 -0500

    Implement email sending functionality with professional formatting

--- server/src/main/java/policonsultorio/demo/controller/emailController.java
+++ server/src/main/java/policonsultorio/demo/controller/emailController.java
@@ -4,16 +4,13 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.eclipse.angus.mail.util.MailConnectException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import policonsultorio.demo.dto.DtoEmail;
-import policonsultorio.demo.config.EmailConfig;
 import policonsultorio.demo.service.EmailService;
 
-import java.util.Map;
+
 
 @RestController
 @RequestMapping("/email")
@@ -24,14 +21,16 @@ public class EmailController {
 
     private final EmailService emailService;
 
+
     @PostMapping("/sendMessage")
     @Operation(
-            summary = "Send email",
-            description = "Send email to user",
+            summary = "Send an email",
+            description = "Send an email with the given data",
             tags = {"email"}
     )
     public ResponseEntity<?> sendEmail(@RequestBody DtoEmail dtoEmail) throws MessagingException {
         emailService.sendEmail(dtoEmail);
         return ResponseEntity.ok("Email sent successfully");
     }
+
 }
